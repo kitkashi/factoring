c9dabea9aa003b9e1731981d1bab34981fac308f
kitkashi <skit.edwards@gmail.com>
2024-10-31 22:24:20 -0500

    added replayability

--- factoringBasics.cpp
+++ factoringBasics.cpp
@@ -4,12 +4,20 @@ using namespace std;
 void findFactors(int);
 
 int main()
-{
+{   char replay;//has to be initialized outside of do while since it doesnt exist outside of
+    //parameters
+    do
+    {
     int num;
     cout<< "Give me a whole number that you would like to know all the factors for: ";
     cin>> num;
     cout<< "This number is factorable by : ";
     findFactors(num);
+    cout<< "\nWould you like to play again? (y/n)";
+    cin.ignore();
+    cin.get(replay);
+    }
+    while(replay=='y'||replay=='Y');
 }
 
 void findFactors(int num)
